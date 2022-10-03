ba922563dca717980a1494cdb7ad32405a339e81
Igor Fernandes <igorfernamdez@gmail.com>
2022-10-02 21:43:16 -0300

    style & validation :art:

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,3 @@
-/**
- * anotações
- * 
- * Criar função para zerar valor/quantidade se clicar num input já marcado
- * Adicionar data de compra quando checkbox for marcado
- * Se pá, criar um metodo de salvar lista pra usar o localStorage
- * 
- * 
- * cada lista terá seu proprio espaco em local storage
- * salvaremos o nome das listas em uma array, para na outra pagina, iterar essa array e pegar cada lista pelo seu nome
- * 
- * a principio funcao de realizar calculo somente na aba de gerenciar listas
- */
-
-
-
-//=================APP
 
 const lista = document.querySelector('.div-list ul')
 let totalItemLista = 0
@@ -29,14 +12,16 @@ document.getElementById('bt-create-item').addEventListener('click', createListIt
 document.querySelector('.pop-up-container.list-name button').addEventListener('click', setListName)
 document.getElementById('bt-save-list').addEventListener('click', saveList)
 document.getElementById('bt-delete-list').addEventListener('click', deleteList)
-
+document.getElementById('input-novo-item').addEventListener('keyup', checkLength)
+// document.querySelector('.input-container button[disabled]').addEventListener('click', ()=> alert('desativado'))
+document.querySelector('.pop-up-container.list-name input').addEventListener('keyup', checkPopUpLength)
 
 
 //funcoes de evento
-function createListItem(){
+function createListItem() {
     const nomeItem = document.getElementById('input-novo-item').value
-    
-    const novoItem = 
+
+    const novoItem =
         `<li>
         <input type="checkbox" class="list-checkbox hide" onclick="check(event)">
         <span class="span-item">${nomeItem}</span>
@@ -50,15 +35,16 @@ function createListItem(){
     totalItemLista === 0 ? showListNamePopUp() : totalItemLista++
 
     document.getElementById('input-novo-item').value = ""
+    document.getElementById('bt-create-item').setAttribute('disabled', '')
 }
 
 
-function showListNamePopUp(){
+function showListNamePopUp() {
     document.querySelector('.pop-up-container.list-name').style.display = 'initial'
 }
 
 
-function setListName(){
+function setListName() {
     listName = document.querySelector('.pop-up-container.list-name input').value
 
     document.querySelector('.texts h5').textContent = document.querySelector('.pop-up-container.list-name input').value
@@ -67,21 +53,22 @@ function setListName(){
     totalItemLista++
 }
 
-function deleteItem(event){
+
+function deleteItem(event) {
     event.target.parentElement.style.display = 'none'
 }
 
 
-function saveList(){
+function saveList() {
 
 
     //add nova lista na lista de nomes do local storage
     let listasCriadas = localStorage.getItem('listasCriadas')
 
     //pra tirar o null se nao tiver nada na lista
-    if(listasCriadas === null) listasCriadas = ''
+    if (listasCriadas === null) listasCriadas = ''
 
-    listasCriadas += listName+","
+    listasCriadas += listName + ","
     localStorage.setItem('listasCriadas', listasCriadas)
 
 
@@ -93,7 +80,25 @@ function saveList(){
     window.location.href = "http://127.0.0.1:5501/Fundamentar/99-Desafios/lista-de-compras/page2.html"
 }
 
-function deleteList(){
+
+function deleteList() {
     window.location.reload()
+}
+
+
+function checkLength(event) {
+    if (event.target.value.length > 2) {
+        document.getElementById('bt-create-item').removeAttribute('disabled')
+    } else{
+        document.getElementById('bt-create-item').setAttribute('disabled', '')
+    }
+}
+
+function checkPopUpLength(event){
+    if (event.target.value.length > 2) {
+        document.querySelector('.pop-up-container.list-name button').removeAttribute('disabled')
+    } else{
+        document.querySelector('.pop-up-container.list-name button').setAttribute('disabled', '')
+    }
 }
 
